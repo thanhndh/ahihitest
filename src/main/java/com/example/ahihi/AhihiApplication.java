277ae6b4d4c89bd34e866d7464133eb08141fc93
thanhndh <thanhndh@bkav.com>
2022-06-29 12:04:23 +0700

    Initial commit

---
+++ src/main/java/com/example/ahihi/AhihiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ahihi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhihiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhihiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ahihi/AhihiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ahihi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhihiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
