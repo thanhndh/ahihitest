f898bb224e94480bb57364d8669954197e2f365a
thanhndh <thanhndh@bkav.com>
2022-06-29 13:49:49 +0700

    add Getmapping

--- src/main/java/com/example/ahihi/AhihiApplication.java
+++ src/main/java/com/example/ahihi/AhihiApplication.java
@@ -12,7 +12,7 @@ public class AhihiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AhihiApplication.class, args);
 	}
-	@GetMapping("/")
+	@GetMapping("/test")
 	public String get(){
 		return "index";
 	}
